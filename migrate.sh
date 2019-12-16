b2168355e0e22a01c6c5c76dbefd9d736ea072ec
macinjoke <shunji.makino@gmail.com>
2019-12-17 00:48:10 +0900

    update

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,2 @@
-rm /Users/makinoshunni/Library/Application\ Support/Code/User/settings.json > /dev/null 2>&1
-ln -s ~/.ghq/github.com/macinjoke/my-vscode-setting/settings.json /Users/makinoshunni/Library/Application\ Support/Code/User/settings.json
+rm ~/Library/Application\ Support/Code/User/settings.json > /dev/null 2>&1
+ln -s ~/.ghq/github.com/macinjoke/my-vscode-setting/settings.json ~/Library/Application\ Support/Code/User/settings.json

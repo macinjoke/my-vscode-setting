7b884b0244c08644de006632dc7b5698b0fe7ef7
macinjoke <shunji.makino@gmail.com>
2019-12-17 00:47:01 +0900

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# my-vscode-setting
+
+自分用の vscode 設定ファイル
+
+# set up
+
+clone this repository, then run migrate.sh
---
+++ migrate.sh
@@ -0,0 +1,2 @@
+rm /Users/makinoshunni/Library/Application\ Support/Code/User/settings.json > /dev/null 2>&1
+ln -s ~/.ghq/github.com/macinjoke/my-vscode-setting/settings.json /Users/makinoshunni/Library/Application\ Support/Code/User/settings.json

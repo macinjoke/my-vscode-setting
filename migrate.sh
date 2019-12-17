d8586c87505dcfab02b8d29cc13e39c5ff178471
macinjoke <shunji.makino@gmail.com>
2019-12-18 00:31:01 +0900

    add keybindings.json

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,4 @@
 rm ~/Library/Application\ Support/Code/User/settings.json > /dev/null 2>&1
+rm ~/Library/Application\ Support/Code/User/keybindings.json > /dev/null 2>&1
 ln -s ~/.ghq/github.com/macinjoke/my-vscode-setting/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.ghq/github.com/macinjoke/my-vscode-setting/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
